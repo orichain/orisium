737abfa2f6f6fd2232f0bad87d632658bd6b1c94
orichain <dhaniprg.2007@gmail.com>
2025-10-05 12:59:02 +0700

    Adding Packet Anchor

--- src/workers/ipc/udp_data/from_sio/cow_heartbeat.c
+++ src/workers/ipc/udp_data/from_sio/cow_heartbeat.c
@@ -116,7 +116,7 @@ status_t handle_workers_ipc_udp_data_sio_heartbeat(worker_context_t *worker_ctx,
             CLOSE_ORILINK_RAW_PROTOCOL(&oudp_datao);
             return cmac;
         }
-        if (!is_same_ctr(&session->packet_anchor.last_rcvd_ctr, session->packet_anchor.last_rcvd_nonce, &oudp_datao->ctr, security->remote_nonce)) {
+        if (!is_same_ctr(&session->packet_anchor.last_rcvd_ctr, session->packet_anchor.last_rcvd_nonce, &oudp_datao->ctr, session->packet_anchor.last_rcvd_nonce)) {
             LOG_ERROR("%sHeartbeat Received Already.", worker_ctx->label);
             CLOSE_IPC_PROTOCOL(&received_protocol);
             CLOSE_ORILINK_RAW_PROTOCOL(&oudp_datao);
