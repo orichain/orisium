b94eb45f7adcb6362da86f3dc702d725f94285f0
orichain <dhaniprg.2007@gmail.com>
2025-10-08 04:09:33 +0700

    Fixing: Heartbeat Timer

--- src/workers/ipc/udp_data/from_sio/cow_heartbeat_ack.c
+++ src/workers/ipc/udp_data/from_sio/cow_heartbeat_ack.c
@@ -39,7 +39,8 @@ status_t handle_workers_ipc_udp_data_sio_heartbeat_ack(worker_context_t *worker_
         }
         LOG_ERROR("%sHeartbeat_Ack Received Already(3). Protocol %d, data_ctr: %u, *ctr: %u", worker_ctx->label, oudp_datao->type, oudp_datao->ctr, security->remote_ctr);
 //----------------------------------------------------------------------
-        cleanup_control_packet(worker_ctx->label, &worker_ctx->async, &session->heartbeat, false);
+        decrement_ctr(&security->local_ctr, security->local_nonce);
+        decrement_ctr(&security->remote_ctr, security->remote_nonce);
 //----------------------------------------------------------------------
         CLOSE_IPC_PROTOCOL(&received_protocol);
         CLOSE_ORILINK_RAW_PROTOCOL(&oudp_datao);
