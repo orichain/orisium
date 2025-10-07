e4eb8d448e00e8274352851f39361cbe0c5068a4
orichain <dhaniprg.2007@gmail.com>
2025-10-08 03:55:42 +0700

    Fixing: Heartbeat Timer

--- src/workers/timer/handlers.c
+++ src/workers/timer/handlers.c
@@ -197,8 +197,10 @@ status_t handle_workers_timer_event(worker_context_t *worker_ctx, void *sessions
                         uint8_t extend_time = session->heartbeat_interval_extended_retrycount;
                         session->heartbeat_interval_extended_retrycount = 0x00;
                         if (extend_time) extend_time = 1;
-                        double timer_interval = (double)extend_time * pow((double)2, (double)session->retry.value_prediction);
+                        double timer_interval = session->heartbeat_interval;
                         timer_interval += session->rtt.value_prediction / (double)1e9;
+                        timer_interval *= session->retry.value_prediction;
+                        timer_interval *= (double)extend_time;
                         LOG_DEVEL_DEBUG("%sRetry Detected. Add Interval To Heartbeat Timer Sender For %fsec", worker_ctx->label, timer_interval);
                         if (async_create_timerfd(worker_ctx->label, &session->heartbeat_sender_timer_fd) != SUCCESS) {
                             return FAILURE;
@@ -382,8 +384,10 @@ status_t handle_workers_timer_event(worker_context_t *worker_ctx, void *sessions
                         uint8_t extend_time = session->heartbeat_interval_extended_retrycount;
                         session->heartbeat_interval_extended_retrycount = 0x00;
                         if (extend_time) extend_time = 1;
-                        double timer_interval = (double)extend_time * pow((double)2, (double)session->retry.value_prediction);
+                        double timer_interval = session->heartbeat_interval;
                         timer_interval += session->rtt.value_prediction / (double)1e9;
+                        timer_interval *= session->retry.value_prediction;
+                        timer_interval *= (double)extend_time;
                         LOG_DEVEL_DEBUG("%sRetry Detected. Add Interval To Heartbeat Timer Sender For %fsec", worker_ctx->label, timer_interval);
                         if (async_create_timerfd(worker_ctx->label, &session->heartbeat_sender_timer_fd) != SUCCESS) {
                             return FAILURE;
