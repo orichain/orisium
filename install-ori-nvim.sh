1507261057286ee815fd7360ff46323345ec3d6e
orichain <dhaniprg.2007@gmail.com>
2026-01-23 12:26:45 +0700

    change ide development to OpenBSD+NeoVim

--- install-ori-nvim.sh
+++ install-ori-nvim.sh
@@ -28,4 +28,4 @@ mkdir -p ~/.local/share/nvim/lazy
 git clone https://github.com/nvim-treesitter/nvim-treesitter.git ~/.local/share/nvim/lazy/nvim-treesitter
 
 echo "==> Done. Launching nvim..."
-exec nvim
+exec nvim src/orisium.c
