c8634c9fb81c32ad485cd6c8cd2162e3bdbcc203
orichain <dhaniprg.2007@gmail.com>
2026-01-23 12:01:43 +0700

    change ide development to OpenBSD+NeoVim

---
+++ install-ori-nvim.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "==> Removing old Neovim config..."
+rm -rf ~/.config/nvim \
+       ~/.local/share/nvim \
+       ~/.local/state/nvim \
+       ~/.cache/nvim
+
+echo "==> Installing packages..."
+doas pkg_add neovim unzip luarocks-lua54 git bear spdlog clang-tools-extra
+
+echo "==> Installing Nerd Font (0xProto)..."
+mkdir -p ~/.local/share/fonts
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/0xProto.zip
+unzip -q 0xProto.zip -d ~/.local/share/fonts/0xProto
+rm 0xProto.zip
+fc-cache -fv
+
+echo "==> Fixing Lua/Luarocks symlinks..."
+doas rm -f /usr/local/bin/luarocks /usr/local/bin/luarocks-admin /usr/local/bin/lua
+doas ln -sf /usr/local/bin/luarocks-5.4 /usr/local/bin/luarocks
+doas ln -sf /usr/local/bin/luarocks-admin-5.4 /usr/local/bin/luarocks-admin
+doas ln -sf /usr/local/bin/lua54 /usr/local/bin/lua
+
+echo "==> Installing Neovim config..."
+git clone --depth 1 https://github.com/orichain/orinvim.git ~/.config/nvim
+mkdir -p ~/.local/share/nvim/lazy
+git clone https://github.com/nvim-treesitter/nvim-treesitter.git ~/.local/share/nvim/lazy/nvim-treesitter
+
+echo "==> Done. Launching nvim..."
+exec nvim
