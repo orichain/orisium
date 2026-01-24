51e0129edf9f50c6ce5274cb8888a887860c17f4
orichain <dhaniprg.2007@gmail.com>
2026-01-24 16:52:32 +0700

    nvim 4 orisium

--- install-ori-nvim.sh
+++ install-ori-nvim.sh
@@ -2,12 +2,12 @@
 
 echo "==> Removing old Neovim config..."
 rm -rf ~/.config/nvim \
-       ~/.local/share/nvim \
-       ~/.local/state/nvim \
-       ~/.cache/nvim
+  ~/.local/share/nvim \
+  ~/.local/state/nvim \
+  ~/.cache/nvim
 
 echo "==> Installing packages..."
-doas pkg_add neovim unzip luarocks-lua54 git bear spdlog clang-tools-extra
+doas pkg_add neovim unzip luarocks-lua51 git bear spdlog clang-tools-extra
 
 echo "==> Installing Nerd Font (0xProto)..."
 mkdir -p ~/.local/share/fonts
@@ -18,9 +18,9 @@ fc-cache -fv
 
 echo "==> Fixing Lua/Luarocks symlinks..."
 doas rm -f /usr/local/bin/luarocks /usr/local/bin/luarocks-admin /usr/local/bin/lua
-doas ln -sf /usr/local/bin/luarocks-5.4 /usr/local/bin/luarocks
-doas ln -sf /usr/local/bin/luarocks-admin-5.4 /usr/local/bin/luarocks-admin
-doas ln -sf /usr/local/bin/lua54 /usr/local/bin/lua
+doas ln -sf /usr/local/bin/luarocks-5.1 /usr/local/bin/luarocks
+doas ln -sf /usr/local/bin/luarocks-admin-5.1 /usr/local/bin/luarocks-admin
+doas ln -sf /usr/local/bin/lua51 /usr/local/bin/lua
 
 echo "==> Installing Neovim config..."
 git clone --depth 1 https://github.com/orichain/orinvim.git ~/.config/nvim
